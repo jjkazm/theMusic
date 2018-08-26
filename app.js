a8966f5828cb23a5ac33bb448ec0883dae96f4f7
Kuba <kazmierczak88@gmail.com>
2018-08-26 14:41:59 +0000

    add mongoose, connect mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express       = require("express"),
     app           = express(),
+    mongoose      = require("mongoose"),
     IndexRoutes   = require("./routes/index.js");
     
     
@@ -7,6 +8,7 @@ var express       = require("express"),
     
 app.set("view engine", "ejs");
 
+mongoose.connect("mongodb://localhost:27017/theMusic", { useNewUrlParser: true });
 
 
 
